e49d04cba09d12d74651e5043891d033923d8a19
Madhuri3199 <emadhuri2021@gmail.com>
2026-07-25 13:10:42 +0530

    updated all content

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Experience } from './pages/experience/experience';
+import { Projects } from './pages/projects/projects';
+import { Skills } from './pages/skills/skills';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'about',
+    component: About,
+  },
+  {
+    path: 'experience',
+    component: Experience,
+  },
+  {
+    path: 'projects',
+    component: Projects,
+  },
+  {
+    path: 'skills',
+    component: Skills,
+  },
+  {
+    path: 'contact',
+    component: Contact,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
